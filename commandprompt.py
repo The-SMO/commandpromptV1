437541212062abc595fda6f99a925c28d3bd2c49
unknown <thesmo.ssb@gmail.com>
2023-05-11 22:55:16 +0100

    fixed bassically everything exept my grammer

--- commandprompt.py
+++ commandprompt.py
@@ -41,32 +41,48 @@ def startline():
     global addextra
     line = line + addextra
     line += 1
-    #print(line)
+    print(line)
 def enter():
+    global line
     lineget1 = str(line) + ".04"
     lineget2 = str(line) + ".90"
     input1 = text.get(lineget1,lineget2)
+    global addextra
     #if input1 == "help":
         #list help from commandhelp.py
-    command = "cmd /c" +"\""+ input1+"\""
-    os.system("cmd /c" +"\""+ input1+"\"")
-    #returned_text = subprocess.check_output("dir", shell=True, universal_newlines=True)
-    #print(returned_text)
-    retext = subprocess.check_output(command,errors=None,shell=None)
-    print(retext)
-    text.insert(tk.END,retext)
-    global addextra
-    addextra = 0
-    addextra = len(retext.splitlines())
-    #text.delete("1.0","1.20")
-    startline()
-    #print(input1)
-    #print(lineget1)
-    #print(lineget2)
+    match input1.lower():
+        case "getline":
+            print(line)
+            text.insert(tk.END,"\n"+str(line)+"\n")
+            text.insert(tk.END,">>> ")
+            line = line + 2
+        case _:
+
+            command = "cmd /c" +"\""+ input1+"\""
+            os.system("cmd /c" +"\""+ input1+"\"")
+            #returned_text = subprocess.check_output("dir", shell=True, universal_newlines=True)
+            #print(returned_text)
+            try:
+                retext = subprocess.check_output(command,errors=None,shell=None)
+                print(retext)
+                text.insert(tk.END,retext)
+                addextra = 0
+                addextra = len(retext.splitlines())
+            except:
+                text.insert(tk.END,"\nCommand "+input1+" is not recognised")
+                #global addextra
+                print(line)
+                line = line + 1
+
+            #text.delete("1.0","1.20")
+            startline()
+            #print(input1)
+            #print(lineget1)
+            #print(lineget2)
 
 enter=tk.Button(window,text="Enter", command = enter)
 enter.pack()
-text.insert(tk.END,"======================= START OF LICENSE NOTICE =======================\n  Copyright (C) 2023 Olivers Sokolovs Kazaks. All Rights Reserved\n\n  NO WARRANTY. THE PRODUCT IS PROVIDED BY DEVELOPER \"AS IS\" AND ANY\n  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DEVELOPER BE LIABLE FOR\n  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE\n  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER\n  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR\n  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE PRODUCT, EVEN\n  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n======================== END OF LICENSE NOTICE ========================\n  Primary Author: Olivers Sokolovs Kazaks\n")
+text.insert(tk.END,"======================= START OF LICENSE NOTICE =======================\n  Copyright (C) 2023 TheSMO. All Rights Reserved\n\n  NO WARRANTY. THE PRODUCT IS PROVIDED BY DEVELOPER \"AS IS\" AND ANY\n  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DEVELOPER BE LIABLE FOR\n  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE\n  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER\n  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR\n  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THE PRODUCT, EVEN\n  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n======================== END OF LICENSE NOTICE ========================\n  Primary Author: TheSMO\n")
 text.insert(tk.END,">>> ")
 line += 16
 tk.mainloop()
